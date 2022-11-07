4099a92b8ad4583eeaf702d3355a747d68e32a02
Tayo Adekoya <Tayoadekoya96@gmail.com>
2022-11-07 14:09:55 -0700

    update

--- main.tf
+++ main.tf
@@ -6,3 +6,10 @@ resource "azurerm_resource_group" "rg_name" {
  name     = var.resource_group_name
  location = var.resource_group_location 
 }
+
+resource "azurerm_virtual_network" "VNet_name" {
+ name                = var.virtual_network_name
+ resource_group_name = var.rg_name.name
+ location            = var.rg_name.name
+ address_space       = ["10.0.0.0/16"]
+}
--- var.tf
+++ var.tf
@@ -7,3 +7,8 @@ variable "resource_group_location" {
     type        = string
     description = "The location of the resource group"
 }
+
+variable "virtual_network_name" {
+    type        = string
+    description = "the name of the Virtual Network"
+}
