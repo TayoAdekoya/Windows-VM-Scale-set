5f0705785e47e560d56f83469b0e8397825f6997
Tayo Adekoya <Tayoadekoya96@gmail.com>
2022-11-08 10:43:32 -0700

    update

--- main.tf
+++ main.tf
@@ -13,3 +13,43 @@ resource "azurerm_virtual_network" "VNet_name" {
  location            = var.resource_group_location
  address_space       = ["10.0.0.0/16"]
 }
+
+resource "azurerm_subnet" "Subnet_name" {
+  name                 = var.subnet_name
+  resource_group_name  = var.resource_group_name
+  virtual_network_name = var.virtual_network_name
+  address_prefixes     = ["10.0.0.0/24"]
+}
+
+resource "azurerm_windows_virtual_machine_scale_set" "VMSS_name" {
+  name                = var.windows_virtual_machine_scale_set_name
+  resource_group_name = var.resource_group_name
+  location            = var.resource_group_location
+  sku                 = var.sku
+  instances           = var.instances
+  admin_password      = var.admin_password
+  admin_username      = var.admin_username
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = var.source_image_reference_sku
+    version   = "latest"
+  }
+
+  os_disk {
+    storage_account_type = var.storage_account_type
+    caching              = "ReadWrite"
+  }
+
+  network_interface {
+    name = var.network_interface_name
+    primary = true
+
+    ip_configuration {
+      name = var.subnet_name
+      primary = true
+      subnet_id = var.subnet_name.id
+    }
+  }
+}
--- var.tf
+++ var.tf
@@ -4,11 +4,56 @@ variable "resource_group_name" {
 }
 
 variable "resource_group_location" {
-    type        = string
-    description = "The location of the resource group"
+  type        = string
+  description = "The location of the resource group"
 }
 
 variable "virtual_network_name" {
-    type        = string 
-    description = "the name of the Virtual Network"
+  type        = string 
+  description = "The name of the Virtual Network"
+}
+
+variable "subnet_name" {
+  type        = string
+  description = "The name of the subnet"
+}
+
+variable "windows_virtual_machine_scale_set_name" {
+  type        = string
+  description = "The name of the Windows VM scale-set"
+}
+
+variable "sku" {
+  type        = string
+  description = "Sku type for Windows VM Scale-set" 
+}
+
+variable "instances" {
+  type        = number
+  description = "Number of instances"
+}
+
+variable "admin_password" {
+  type        = string
+  description = "Admin password"
+}
+
+variable "admin_username" {
+  type        = string
+  description = "Admin Password"
+}
+
+variable "source_image_reference_sku" {
+  type        = string
+  description = "The Sku for the source image 2012, 2016, 2019 datacenter server core"
+}
+
+variable "storage_account_type" {
+  type        = string
+  description = "Storage account type for OS disk"
+}
+
+variable "network_interface_name" {
+  type        = string
+  description = "The name for the NEtwork Interface"
 }
